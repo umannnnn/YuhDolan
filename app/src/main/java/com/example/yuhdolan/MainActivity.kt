bd996e2584303ec9f9450f6fc527b169cde22ed6
hilmyha <hilmyahmadhaidar@gmail.com>
2023-05-13 21:47:54 +0700

    update | Update fragment

--- app/src/main/java/com/example/yuhdolan/MainActivity.kt
+++ app/src/main/java/com/example/yuhdolan/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.yuhdolan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.yuhdolan.Fragment.DestinationFragment
+import com.example.yuhdolan.Fragment.HomeFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val destinationFragment = DestinationFragment()
+
+        val navbar = findViewById<BottomNavigationView>(R.id.navbar)
+
+        makeCurrentFragment(homeFragment)
+
+        navbar.setOnNavigationItemReselectedListener {
+            when(it.itemId) {
+                R.id.nav_home -> makeCurrentFragment(homeFragment)
+                R.id.nav_destination -> makeCurrentFragment(destinationFragment)
+            }
+        }
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.navbar_frame, fragment)
+            commit()
+        }
     }
 }
